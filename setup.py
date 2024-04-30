52ae337f9ce0ad083adc9dc4e42b9e7d7e24cc94
pvshvp-oss <pvshvp.oss@gmail.com>
2024-04-29 21:42:14 -0500

    Bump versions and add contributing checklist

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyrunning",
-    version="0.0.13",
+    version="0.0.14",
     author="shiva.patt",
     author_email="shiva.patt.oss@gmail.com",
     description="A python library to run and live-log OS commands, functions, scripts, and batch jobs either immedietly, or to be queued for later execution.",
